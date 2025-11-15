1716f4ee897ffd13eaf8bc1c9cc110a3ed6b2230
uemuraj <uemura.junya@gmail.com>
2025-11-16 08:53:36 +0900

    プロジェクト ファイルを追加します。

---
+++ slacky/winmain.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+_Use_decl_annotations_
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	(void) hInstance; (void) hPrevInstance; (void) pCmdLine; (void) nCmdShow;
+
+	return 0;
+}
